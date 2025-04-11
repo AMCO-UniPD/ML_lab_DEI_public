6a6c3c266c776f1589d7b4dde9e0e0b67c400aa3
francesco-borsatti-unipd <francesco.borsatti.1@studenti.unipd.it>
2025-04-11 11:18:46 +0200

    utils

--- utils.py
+++ utils.py
@@ -6,9 +6,15 @@ import plotly.graph_objects as go
 import ipywidgets as widgets
 import matplotlib.cm as cm
 import matplotlib.colors as mcolors
-import numpy as np
 import matplotlib.pyplot as plt
 from ipywidgets import interact
+from sklearn.datasets import make_blobs
+
+from typing import Optional
+import matplotlib.animation as animation
+from matplotlib.colors import to_rgba
+from IPython.display import HTML, display
+
 
 
 def get_interactive_plot_pca():
@@ -327,7 +333,9 @@ def create_2d_meshpoints(X, resolution=200):
     return mesh_points, xx, yy, X
 
 
-def plot_decision_boundary_2d(X_grid, y, prob_function, xx, yy, X_2d, n_features, title=None):
+def plot_decision_boundary_2d(
+    X_grid, y, prob_function, xx, yy, X_2d, n_features, title=None
+):
     """
     Plots the decision boundary of a logistic regression model as a black line
     dividing the classes space shaded by a color map.
@@ -528,54 +536,146 @@ def plot_combined_probability_boundary(
     plt.tight_layout()
     plt.show()
 
-def stratified_train_test_split(X, y, train_prop): 
+
+def stratified_train_test_split(X, y, train_prop):
     """
     Split randomly the dataset into training and test sets while preserving class proportions.
 
-    Args: 
+    Args:
     X: array of samples. Shape (n_samples, n_features)
     y: array of labels. Shape (n_samples, ) or (n_samples, 1)
     train_prop: proportion of samples to include in the training set. Must be between 0 and 1.
 
-    Returns: 
+    Returns:
     X_train: array of training samples. Shape (n_train_samples, n_features)
     X_test: array of test samples. Shape (n_test_samples, n_features)
     y_train: array of training labels. Shape (n_train_samples, ) or (n_train_samples, 1)
     y_test: array of test labels. Shape (n_test_samples, ) or (n_test_samples, 1)
     """
-    unique_classes = np.unique(y)                           # get unique class labels (i.e., the different classes)
+    unique_classes = np.unique(
+        y
+    )  # get unique class labels (i.e., the different classes)
 
-    train_indices = []                     
+    train_indices = []
     test_indices = []
 
-    for cls in unique_classes: 
-        indices = np.where(y == cls)[0]                     # find indices of samples belonging to class cls
-        np.random.shuffle(indices)                          # shuffle the indices for randomness
-        split_idx = int(indices.shape[0] * train_prop)      # determine split index 
+    for cls in unique_classes:
+        indices = np.where(y == cls)[
+            0
+        ]  # find indices of samples belonging to class cls
+        np.random.shuffle(indices)  # shuffle the indices for randomness
+        split_idx = int(indices.shape[0] * train_prop)  # determine split index
 
-        cls_train_indices = indices[:split_idx].tolist()    # get the first split_idx indices for training for class cls
-        cls_test_indices = indices[split_idx:].tolist()     # get the remaining indices for testing for class cls
+        cls_train_indices = indices[
+            :split_idx
+        ].tolist()  # get the first split_idx indices for training for class cls
+        cls_test_indices = indices[
+            split_idx:
+        ].tolist()  # get the remaining indices for testing for class cls
 
-        train_indices = train_indices + cls_train_indices   # concatenate the training indices for all classes
-        test_indices = test_indices + cls_test_indices      # concatenate the test indices for all classes
+        train_indices = (
+            train_indices + cls_train_indices
+        )  # concatenate the training indices for all classes
+        test_indices = (
+            test_indices + cls_test_indices
+        )  # concatenate the test indices for all classes
 
     # shuffle again to avoid unintended ordering (right now the indices are ordered by class)
     np.random.shuffle(train_indices)
     np.random.shuffle(test_indices)
-    
+
     X_train, y_train = X[train_indices], y[train_indices]
     X_test, y_test = X[test_indices], y[test_indices]
     return X_train, X_test, y_train, y_test
 
 
-def accuracy(y_true, y_pred): 
+def accuracy(y_true, y_pred):
     """
     Args:
     y_true: array of true labels. Shape (n_samples, ) or (n_samples, 1)
     y_pred: array of predicted labels. Shape (n_samples, ) or (n_samples, 1)
     """
     total_predictions = len(y_true)
-    is_correct = (y_true == y_pred)
+    is_correct = y_true == y_pred
     n_correct_pred = np.sum(is_correct)
     accuracy = n_correct_pred / total_predictions
     return accuracy
+
+
+def generate_blobs_dataset(n_samples, n_features, n_classes, seed, phi=0.0):
+    np.random.seed(seed)
+    angles = np.linspace(phi, 2 * np.pi + phi, n_classes, endpoint=False)
+    centers = np.column_stack((np.cos(angles), np.sin(angles))) * 10
+    data = make_blobs(
+        n_samples=n_samples,
+        centers=centers,
+        n_features=n_features,
+        random_state=1,
+        cluster_std=2,
+    )
+    assert isinstance(data, tuple) and len(data) == 2
+    X, y = data
+    return X, y
+
+
+def centroids_animation(
+    X,
+    centroids,
+    labels,
+    max_frames: Optional[int] = None,
+    point_darkening=True,
+    marker_size=100,
+    t_interval=200,
+):
+    n_classes, n_centroids, n_features = centroids.shape
+
+    # find ranges of the data
+    Xx = np.concatenate((X, centroids.reshape(-1, centroids.shape[2])), axis=0)
+    x_min, x_max = Xx[:, 0].min(), Xx[:, 0].max()
+    y_min, y_max = Xx[:, 1].min(), Xx[:, 1].max()
+    x_margin = (x_max - x_min) * 0.1  # add a 10% margin
+    y_margin = (y_max - y_min) * 0.1
+
+    fig, ax = plt.subplots()  # create figure
+    plt.tight_layout()
+    scatter = ax.scatter(X[:, 0], X[:, 1], alpha=0.1)  # background scatter plot of dataset
+    ax.set_xlim(x_min - x_margin, x_max + x_margin)
+    ax.set_ylim(y_min - y_margin, y_max + y_margin)
+
+    # each class has: 1. a point, 2. a line that connects the points
+    current_points, lines = [], []
+    for i in range(n_classes):
+        current_point = ax.scatter([], [], color=f"C{i}", marker="o", s=marker_size)
+        (line,) = ax.plot([], [], color=f"C{i}", alpha=0.7)  # line
+        current_points.append(current_point)
+        lines.append(line)
+
+    max_frames = n_centroids if max_frames is None else max_frames
+    max_frames = min(max_frames, n_centroids)
+
+    def update(frame_num):
+        scatter.set_color([f"C{i}" for i in labels[frame_num]])
+        for i in range(n_classes):  # Loop over each class
+            x = centroids[i, : frame_num + 1, 0]
+            y = centroids[i, : frame_num + 1, 1]
+            # Update the current point for class i
+            data = np.stack([x, y], axis=1)
+            current_points[i].set_offsets(data)
+            base_color = np.array(to_rgba(f"C{i}")[:3])
+            if point_darkening:
+                # Age: newest point has age = 0, oldest = frame_num
+                ages = frame_num - np.arange(frame_num + 1)
+                darkness = np.clip(1 - ages / max_frames, 0, 1)
+                colors = np.outer(darkness, base_color)
+            else:
+                colors = np.tile(base_color, (frame_num + 1, 1))
+            current_points[i].set_color(colors)
+            lines[i].set_data(x, y)  # Update the line for class i
+        return current_points + lines
+
+    # Show the animation
+    anim = animation.FuncAnimation(
+        fig, update, frames=max_frames, interval=t_interval, repeat=False
+    )
+    display(HTML(anim.to_jshtml()))  # Display the video
+    plt.close(fig)
