0e166d81df15da4468e70d5c394da87b0959379d
FrancescoBorsatti <francesco.borsatti.1@studenti.unipd.it>
2025-05-31 12:21:39 +0200

    update

--- update.sh
+++ update.sh
@@ -1,2 +1,35 @@
 #!/bin/bash
+
+# check if environment variable EXAM is set
+if [ -n "$EXAM" ]; then
+
+    git checkout exam
+    git fetch origin exam
+    git pull origin exam
+
+    tar_files=(*.tar.gz)
+    echo "Select a file to extract:"
+    select tar_file in "${tar_files[@]}"; do
+        if [ -n "$tar_file" ]; then
+            break
+        else
+            echo "Invalid selection. Please try again."
+        fi
+    done
+
+    openssl enc -aes-256-cbc -pbkdf2 -iter 100000 -d -in "$tar_file" -pass pass:"$EXAM" | tar -xz -C .
+
+    # download slides
+    curl -o ML_exam_slides.tar.xz https://cloud.dei.unipd.it/public.php/dav/files/qiiccDGqZREDbHB
+
+    # check if the download was successful
+    if [ $? -ne 0 ]; then
+        echo "Failed to download slides. Please check your internet connection."
+    else
+        tar -xf ML_exam_slides.tar.xz
+        rm ML_exam_slides.tar.xz
+    fi
+fi
+
+# start Jupyter Lab
 jupyter lab
