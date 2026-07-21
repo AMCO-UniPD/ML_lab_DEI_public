9396c9b1f8e8bc3de0688a88b241bb799620dfff
zaccariavalentina <108325151+zaccariavalentina@users.noreply.github.com>
2026-07-21 14:33:34 +0200

    soluzioni secondo appello

---
+++ exams/2026-07-03_2nd_session/exam_utils.py
@@ -0,0 +1,126 @@
+import numpy as np
+
+
+def random_train_test_split(X, y, proportion_train):
+    split_point = int(
+        proportion_train * X.shape[0]
+    )  # numer of samples in the training set
+
+    shuffled_indices = np.random.permutation(X.shape[0])
+
+    train_indices = shuffled_indices[:split_point]
+    test_indices = shuffled_indices[split_point:]
+
+    X_train, y_train = X[train_indices], y[train_indices]
+    X_test, y_test = X[test_indices], y[test_indices]
+
+    return X_train, X_test, y_train, y_test
+
+
+def stratified_train_test_split(X, y, train_prop):
+    """
+    Split randomly the dataset into training and test sets while preserving class proportions.
+
+    Args:
+    X: array of samples. Shape (n_samples, n_features)
+    y: array of labels. Shape (n_samples, ) or (n_samples, 1)
+    train_prop: proportion of samples to include in the training set. Must be between 0 and 1.
+
+    Returns:
+    X_train: array of training samples. Shape (n_train_samples, n_features)
+    X_test: array of test samples. Shape (n_test_samples, n_features)
+    y_train: array of training labels. Shape (n_train_samples, ) or (n_train_samples, 1)
+    y_test: array of test labels. Shape (n_test_samples, ) or (n_test_samples, 1)
+    """
+    unique_classes = np.unique(
+        y
+    )  # get unique class labels (i.e., the different classes)
+
+    train_indices = []
+    test_indices = []
+
+    for cls in unique_classes:
+        indices = np.where(y == cls)[
+            0
+        ]  # find indices of samples belonging to class cls
+        np.random.shuffle(indices)  # shuffle the indices for randomness
+        split_idx = int(indices.shape[0] * train_prop)  # determine split index
+
+        cls_train_indices = indices[
+            :split_idx
+        ].tolist()  # get the first split_idx indices for training for class cls
+        cls_test_indices = indices[
+            split_idx:
+        ].tolist()  # get the remaining indices for testing for class cls
+
+        train_indices = (
+            train_indices + cls_train_indices
+        )  # concatenate the training indices for all classes
+        test_indices = (
+            test_indices + cls_test_indices
+        )  # concatenate the test indices for all classes
+
+    # shuffle again to avoid unintended ordering (right now the indices are ordered by class)
+    np.random.shuffle(train_indices)
+    np.random.shuffle(test_indices)
+
+    X_train, y_train = X[train_indices], y[train_indices]
+    X_test, y_test = X[test_indices], y[test_indices]
+    return X_train, X_test, y_train, y_test
+
+
+def accuracy(y_true, y_pred): 
+    """
+    Args:
+    y_true: array of true labels. Shape (n_samples, ) or (n_samples, 1)
+    y_pred: array of predicted labels. Shape (n_samples, ) or (n_samples, 1)
+    """
+    total_predictions = len(y_true)
+    is_correct = (y_true == y_pred)
+    n_correct_pred = np.sum(is_correct)
+    accuracy = n_correct_pred / total_predictions
+    return accuracy
+
+def get_confusion_matrix(y_true, y_pred, classes): 
+    """
+    Args:
+    y_true: array of true labels. Shape (n_samples, ) or (n_samples, 1)
+    y_pred: array of predicted labels. Shape (n_samples, ) or (n_samples, 1)
+    classes: list of class labels
+    """
+    n_classes = len(classes)
+    confusion_matrix = np.zeros((n_classes, n_classes))
+    for i in range(n_classes):
+        for j in range(n_classes):
+            confusion_matrix[i, j] = np.sum((y_true == classes[i]) & (y_pred == classes[j]))
+    return confusion_matrix
+
+def TP_TN_FP_FN(y_true, y_pred): 
+    """ 
+    Intuitive way to calculate TP, TN, FP, FN
+    """
+    positive = (y_true == 1)            # array of the same shape as y_true, with True if y_true is pos/1 and False otherwise
+    negative = (y_true == 0)            # array of the same shape as y_true, with True if y_true is neg/0 and False otherwise
+    pred_positive = (y_pred == 1)       # array with True if the predicted label is pos/1, 0 otherwise
+    pred_negative = (y_pred == 0)       # array with True if the predicted label is neg/0, 0 otherwise
+
+    # element-wise boolean operation that returns 1 if both are True/1, 0 otherwise
+    TP = np.sum(positive & pred_positive)  # True Positives
+    TN = np.sum(negative & pred_negative)  # True Negatives
+    FP = np.sum(negative & pred_positive)  # False Positives
+    FN = np.sum(positive & pred_negative)  # False Negatives
+    return TP, TN, FP, FN
+
+
+def specificity(y_true, y_pred): 
+    TP, TN, FP, FN = TP_TN_FP_FN(y_true, y_pred)
+    return TN / (TN + FP)
+
+def precision(y_true, y_pred):
+    TP, TN, FP, FN = TP_TN_FP_FN(y_true, y_pred)
+    return TP / (TP + FP)
+
+def recall(y_true, y_pred):
+    TP, TN, FP, FN = TP_TN_FP_FN(y_true, y_pred)
+    return TP / (TP + FN)
+
